b9add07e00cc79f7a87481ad25d974ed8e63c59d
StevenTse759 <steventse4000@gmail.com>
2022-12-11 17:41:38 +0800

    admin create event

--- src/Home.js
+++ src/Home.js
@@ -14,6 +14,7 @@ export default class Home extends React.Component {
     this.state = {
       loggedIn: this.props.loggedIn === undefined ? false : this.props.loggedIn,
       isAdmin: this.props.isAdmin === undefined ? false : this.props.isAdmin,
+      c: false,
       r: false,
       u: false,
       d: false,
@@ -24,12 +25,16 @@ export default class Home extends React.Component {
     console.log(vars.loggedIn);
 
     this.handleLogout = this.handleLogout.bind(this);
+    this.handleC = this.handleC.bind(this);
     this.handleR = this.handleR.bind(this);
     this.handleU = this.handleU.bind(this);
     this.handleD = this.handleD.bind(this);
   }
   handleLogout() {
     this.setState({ loggedIn: false });
+  }
+  handleC() {
+    this.setState({ c: !this.state.c });
   }
   handleR() {
     this.setState({ r: !this.state.r });
@@ -69,11 +74,12 @@ export default class Home extends React.Component {
               <Link to="/">
                 <button onClick="{this.handleLogout}">Logout</button>
               </Link>
-              <button>Create event</button>
+              <button onClick={this.handleC}>Create event</button>
               <button onClick={this.handleR}> Retrieve events</button>
               <button onClick={this.handleU}>Update event</button>
               <button onClick={this.handleD}>Delete event</button>
             </div>
+            <div>{this.state.c ? <CreateData /> : <p></p>}</div>
             <div>{this.state.r ? <RetrieveData /> : <p></p>}</div>
             <div>{this.state.u ? <UpdateData /> : <p></p>}</div>
             <div>{this.state.d ? <DeleteData /> : <p></p>}</div>
@@ -637,6 +643,141 @@ class UpdateData extends React.Component {
               <p id="updatemessage"></p>
             </div>
           </form>
+        </section>
+      </div>
+    );
+  }
+}
+
+class CreateData extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      venueId: "",
+      datetime: "",
+      description: "",
+      presenter: "",
+      price: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    let newData = {
+      title: document.getElementById("title").value,
+      venueid: Number(document.getElementById("venueId").value),
+      datetime: document.getElementById("datetime").value,
+      description: document.getElementById("description").value,
+      presenter: document.getElementById("presenter").value,
+      price: document.getElementById("price").value,
+    };
+    fetch("http://localhost:8889/create", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(newData),
+    })
+      .then((res) => res.text())
+      .then((txt) => {
+          document.getElementById("createmessage").innerHTML =txt;
+      });  
+  }
+
+  render() {
+    return (
+      <div className="col-sm-12 col-md-12 col-lg-12 m-auto">
+        <section className="p-1 mx-1 border border-primary rounded-1">
+          <h4>Create Data</h4>
+
+          <form className="form" method="POST">
+            <div className="p-4 col-6 m-auto border border-4 border-primary rounded-3">
+              <h3>Create Event</h3>
+
+              <div className="p-2 form-group mt-3">
+                <label htmlFor="title">Title:</label>
+                <input
+                  id="title"
+                  name="title"
+                  type="text"
+                  onChange={this.handleChange}
+                  className="form-control mt-1"
+                  required
+                />
+              </div>
+
+              <div className="p-2 form-group mt-3">
+                <label htmlFor="venueId">Venue ID:</label>
+                <input
+                  id="venueId"
+                  name="venueId"
+                  type="number"
+                  onChange={this.handleChange}
+                  className="form-control mt-1"
+                  required
+                />
+              </div>
+
+              <div className="p-2 form-group mt-3">
+                <label htmlFor="venueName">Datetime:</label>
+                <input
+                  id="datetime"
+                  name="datetime"
+                  type="text"
+                  onChange={this.handleChange}
+                  className="form-control mt-1"
+                  required
+                />
+              </div>
+
+              <div className="p-2 form-group mt-3">
+                <label htmlFor="venueName">Description:</label>
+                <input
+                  id="description"
+                  name="description"
+                  type="text"
+                  onChange={this.handleChange}
+                  className="form-control mt-1"
+                  required
+                />
+              </div>
+
+              <div className="p-2 form-group mt-3">
+                <label htmlFor="venueName">Presenter:</label>
+                <input
+                  id="presenter"
+                  name="presenter"
+                  type="text"
+                  onChange={this.handleChange}
+                  className="form-control mt-1"
+                  required
+                />
+              </div>
+
+              <div className="p-2 form-group mt-3">
+                <label htmlFor="venueName">Price:</label>
+                <input
+                  id="price"
+                  name="price"
+                  type="text"
+                  onChange={this.handleChange}
+                  className="form-control mt-1"
+                  required
+                />
+              </div>
+              <div className="p-2 d-grid gap-2 mt-3">
+                <button onClick={this.handleSubmit} className="btn btn-primary">
+                  Create
+                </button>
+              </div>
+              <p id="createmessage"></p>
+            </div>
+          </form>
+
         </section>
       </div>
     );
