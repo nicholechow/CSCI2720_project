8bae9a458bb93ac6b659213507e2289323deb117
Nichole Chow <nicolechow08@gmail.com>
2022-12-11 11:41:06 +0800

    merged the new navbar with the old one

--- src/App.js
+++ src/App.js
@@ -27,12 +27,22 @@ class App extends React.Component {
     e.preventDefault();
     if (this.state.username === "admin") {
       if (this.state.password === "admin") {
-        this.setState({ adminflag: true, loginflag: true, displaytext: "You are an admin, click here to go to the admin page.", displaylink: "/admin" });
+        this.setState({
+          adminflag: true,
+          loginflag: true,
+          displaytext: "You are an admin, click here to go to the admin page.",
+          displaylink: "/admin",
+        });
         document.getElementById("123").innerText = "";
       }
     } else if (this.state.username === "user") {
       if (this.state.password === "user") {
-        this.setState({ adminflag: false, loginflag: true, displaytext: "You are a user, click here to go to the user page.", displaylink: "/user" });
+        this.setState({
+          adminflag: false,
+          loginflag: true,
+          displaytext: "You are a user, click here to go to the user page.",
+          displaylink: "/user",
+        });
         document.getElementById("123").innerText = "";
       }
     } else {
@@ -41,59 +51,120 @@ class App extends React.Component {
       document.getElementById("123").classList.add("text-danger");
     }
   }
-  render(){
-  return (
-    <>
-      <BrowserRouter>
-        <div className="nav justify-content-between bg-warning sticky-top">
-          <div>
-            <Link to="/">
-              <button className="btn btn-transparent">
+  render() {
+    return (
+      <>
+        <BrowserRouter>
+          <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="/">
                 CSCI2720 Group 19's Project
+              </a>
+              <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span class="navbar-toggler-icon"></span>
               </button>
-            </Link>
-          </div>
-          {vars.username
-            ? '<div><Link to="/debug"><button className="btn btn-transparent">' +
-              vars.username +
-              "</button></Link></div>"
-            : ""}
-          <div>
-            {vars.loggedIn ? (
-              <Link to="/logout">
-                <button className="btn btn-transparent">Log Out</button>
-              </Link>
-            ) : (
-              <Link to="/debug">
-                <button className="btn btn-transparent">Debug Login</button>
-              </Link>
-            )}
-          </div>
-        </div>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/">
+                      Home
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">
+                      Link
+                    </a>
+                  </li>
+                </ul>
+                <div className="nav justify-content-between bg-warning sticky-top">
+                  {vars.username
+                    ? '<div><Link to="/debug"><button className="btn btn-transparent">' +
+                      vars.username +
+                      "</button></Link></div>"
+                    : ""}
+                  <div>
+                    {vars.loggedIn ? (
+                      <Link to="/logout">
+                        <button className="btn btn-transparent">Log Out</button>
+                      </Link>
+                    ) : (
+                      <Link to="/debug">
+                        <button className="btn btn-transparent">
+                          Debug Login
+                        </button>
+                      </Link>
+                    )}
+                  </div>
+                </div>
+                <form class="d-flex" role="search">
+                  <input
+                    class="form-control me-2"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                  ></input>
+                  <button class="btn btn-outline-success" type="submit">
+                    Search
+                  </button>
+                </form>
+              </div>
+            </div>
+          </nav>
 
-        <hr className="m-0 mb-3" />
+          <hr className="m-0 mb-3" />
 
-        <Routes>
-          {/* <Route path="/login/signup" element={<Login mode={signup}/>} /> */}
-          <Route path="/login/signin" element={<Login data={{
-            username: this.state.usernmae,
-            password: this.state.password,
-            displaylink: this.state.displaylink,
-            displaytext: this.state.displaytext,
-            adminflag: this.state.adminflag,
-            loginflag: this.state.loginflag,
-            handleChange: this.handleChange.bind(this),
-            handleSubmit: this.handleSubmit.bind(this)
-          }}/>} />
-          <Route path="/debug" element={<Home loggedIn={true} />} />
-          <Route path="/venue/:venueId" element={<Venue />} />
-          <Route path="/user" element={<Home loggedIn={this.state.loginflag} isAdmin={this.state.adminflag}/>} />
-          <Route path="/admin" element={<Home loggedIn={this.state.loginflag} isAdmin={this.state.adminflag}/>} />
-          <Route path="*" element={<Home loggedIn={false}/>} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+          <Routes>
+            {/* <Route path="/login/signup" element={<Login mode={signup}/>} /> */}
+            <Route
+              path="/login/signin"
+              element={
+                <Login
+                  data={{
+                    username: this.state.usernmae,
+                    password: this.state.password,
+                    displaylink: this.state.displaylink,
+                    displaytext: this.state.displaytext,
+                    adminflag: this.state.adminflag,
+                    loginflag: this.state.loginflag,
+                    handleChange: this.handleChange.bind(this),
+                    handleSubmit: this.handleSubmit.bind(this),
+                  }}
+                />
+              }
+            />
+            <Route path="/debug" element={<Home loggedIn={true} />} />
+            <Route path="/venue/:venueId" element={<Venue />} />
+            <Route
+              path="/user"
+              element={
+                <Home
+                  loggedIn={this.state.loginflag}
+                  isAdmin={this.state.adminflag}
+                />
+              }
+            />
+            <Route
+              path="/admin"
+              element={
+                <Home
+                  loggedIn={this.state.loginflag}
+                  isAdmin={this.state.adminflag}
+                />
+              }
+            />
+            <Route path="*" element={<Home loggedIn={false} />} />
+          </Routes>
+        </BrowserRouter>
+      </>
+    );
   }
 }
 // App;
